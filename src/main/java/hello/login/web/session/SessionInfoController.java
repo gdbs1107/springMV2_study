a17bb329ae0f6f01ee39cd819d0baa0766113e4f
전재연 <sunrox1107@gmail.com>
2024-09-04 18:13:30 +0900

    세션 타임아웃 구현

--- src/main/java/hello/login/web/session/SessionInfoController.java
+++ src/main/java/hello/login/web/session/SessionInfoController.java
@@ -4,10 +4,32 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Date;
+
 @Slf4j
 @RestController
 public class SessionInfoController {
 
     @GetMapping("/session-info")
-    public String sessionInfo
+    public String sessionInfo(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "세션이 없습니다";
+        }
+
+
+        //세션 데이터들 출력
+        session.getAttributeNames().asIterator()
+                .forEachRemaining(name->log.info("{}={}", name, session.getAttribute(name)));
+
+        log.info("sessionId={}",session.getId());
+        log.info("sessionId={}",session.getMaxInactiveInterval());
+        log.info("sessionId={}",new Date(session.getCreationTime()));
+        log.info("sessionId={}",new Date(session.getLastAccessedTime()));
+        log.info("sessionId={}",session.isNew());
+
+        return "세션정보 출력";
+    }
 }
